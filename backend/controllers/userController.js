70ba0b8269547f95263615d66159265d62e14184
Kavvyaa <157046519+Kavvyaa@users.noreply.github.com>
2026-08-11 22:54:35 +0530

    signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,39 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const {mongoClient} = require('mongodb');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const url = process.env.MONGO_URL;
+
+let client;
+async function connectClient() {
+    if (!client) {
+        client = new mongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
+        await client.connect();
+    }
+}
+
 const getAllUsers = (req, res)=>{
     res.send("All users!!");
 };
 
 const signup = (req, res)=>{
+    const {username, email, password} = req.body;
+    try {
+        await connectClient();
+        const db = client.db("Git-Clone");
+        const usersCollection = db.collection("users");
+        const user = await usersCollection.findOne({username});
+        if(user){
+            return res.status(400).json({message: "user already exist"});
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);   
+    } catch (error) {
+        
+    }
     res.send("signing up!!");
 };
 
