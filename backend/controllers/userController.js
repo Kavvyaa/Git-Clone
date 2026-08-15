c6138107e8e0306e2ab76187cfdf9bcab8a4df62
Kavvyaa <157046519+Kavvyaa@users.noreply.github.com>
2026-08-15 14:57:15 +0530

    Login functionality completed using JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -50,8 +50,27 @@ async function signup(req, res) {
     }
 };
 
-const login = (req, res)=>{
-    res.send("loging in!!");
+async function login(req, res){
+    const {email, password} = req.body;
+    try {
+        await connectClient();
+        const db = client.db("Git-Clone");
+        const usersCollection = db.collection("users");
+        const user = await usersCollection.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET_KEY, {expiresIn: "1h"});
+        res.json({token, userId: user._id});
+    } catch (error) {
+        console.error("error while login:", error.message);
+        res.status(500).send("Server Error");
+    }
 };
 
 const getUserProfile = (req, res)=>{
