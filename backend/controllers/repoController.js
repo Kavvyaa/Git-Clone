8ca9c207098e3b154d24c988dc05f3ea98575b5e
Kavvyaa <157046519+Kavvyaa@users.noreply.github.com>
2026-08-15 23:27:20 +0530

    repositories created and fetched

--- backend/controllers/repoController.js
+++ backend/controllers/repoController.js
@@ -1,9 +1,36 @@
-const createRepo = (req, res)=>{
-    res.send("new Repository created!!");
+const mongoose = require('mongoose');
+const Repository = require('../models/repoModel');
+const User = require('../models/userModel');
+const Issue = require('../models/issueModel');
+
+async function createRepo(req, res) {
+    const {owner, name, issues, content, description, visibility} = req.body;
+
+    try {
+        if(!name){
+            res.status(400).json({error: "Repository name is required"});
+        }
+        if(!mongoose.Types.ObjectId.isValid(owner)){
+            res.status(400).json({error: "User is required"});
+        }
+        const newRepo = new Repository({name, description, visibility, issues, content, owner});
+        const result = await newRepo.save();
+        res.status(201).json({message: "Repository created", name: result._id})
+
+    } catch (error) {
+        console.error("Error while creating repo:" , error);
+        res.status(500).send("Server Error1")
+    }
 };
 
-const allRepo = (req, res)=>{
-    res.send("All Repositories!!");
+async function allRepo(req, res){
+    try {
+        const repositories = await Repository.find({}).populate("owner").populate("issues");
+        res.json(repositories);
+    } catch (error) {
+        console.error("Error while creating repo:" , error);
+        res.status(500).send("Server Error1")
+    }
 };
 
 const getRepoById = (req, res)=>{
