5419a1278eb275c30f4dda1e59e06a32c2c93313
Kavvyaa <157046519+Kavvyaa@users.noreply.github.com>
2026-08-17 22:39:40 +0530

    basic project structure

---
+++ frontend/src/authContext.jsx
@@ -0,0 +1,7 @@
+import React, {createContext, useState, useEffect, useContext} from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = ()=>{
+    return useContext(AuthContext);
+}
